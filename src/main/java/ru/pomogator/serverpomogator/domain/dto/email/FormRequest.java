7f73b4ba96682f40fd0335dfae0273add99d685f
Nazarov-Dmitriy <Difat1@inbox.ru>
2025-02-07 00:10:31 +0300

    fix: FormRequest

--- src/main/java/ru/pomogator/serverpomogator/domain/dto/email/FormRequest.java
+++ src/main/java/ru/pomogator/serverpomogator/domain/dto/email/FormRequest.java
@@ -3,18 +3,10 @@ package ru.pomogator.serverpomogator.domain.dto.email;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Value;
 
 
-@Value
-public class FormRequest {
-    @NotBlank(message = "Поле не должно быть пустым")
-    String name;
-    @NotBlank(message = "Поле не должно быть пустым")
-    String phone;
-    @NotBlank(message = "Поле не должно быть пустым")
-    @Email(message = "Email адрес должен быть в формате user@example.ru")
-    String email;
-    @NotEmpty(message = "Поле не должно быть пустым")
-    String question;
+public record FormRequest(@NotBlank(message = "Поле не должно быть пустым") String name,
+                          @NotBlank(message = "Поле не должно быть пустым") String phone,
+                          @NotBlank(message = "Поле не должно быть пустым") @Email(message = "Email адрес должен быть в формате user@example.ru") String email,
+                          @NotEmpty(message = "Поле не должно быть пустым") String question) {
 }
